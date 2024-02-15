bbfd7ad10a9f4dac07a33883c0a9533b39d43823
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-15 08:35:47 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# FIAP-NODE
+Este projeto tem o intuito de aprender como publicar pacotes npm.
+## Título 2
+ihihihihi
---
+++ index.js
@@ -0,0 +1,17 @@
+const sum = (n1, n2) => {
+    return n1 + n2;
+}
+
+const minus = (n1, n2) => {
+    return n1 - n2;
+}
+
+const mult = (n1, n2) => {
+    return n1 * n2;
+}
+
+const divide = (n1, n2) => {
+    return n1 / n2;
+}
+
+module.exports = {sum, minus, mult, divide};
